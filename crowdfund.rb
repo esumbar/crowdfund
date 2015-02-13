fbdd8a52e982def8fdbcb67eb7f46287c017cfb3
Edmund Sumbar <esumbar@gmail.com>
2015-02-12 21:54:48 -0700

    Initial commit

---
+++ crowdfund.rb
@@ -0,0 +1,9 @@
+project1 = 'ABC'
+project2 = 'LMN'
+project3 = 'XYZ'
+
+funding1 = 1000
+
+puts "Project #{project1} has $#{funding1} in funding."
+puts "\n"
+puts "Projects: \n\tProject #{project1}\n\tProject #{project2}\n\tProject #{project3}"
